706109d4a714a2637d90828c595967625629f1cb
Massimiliano Galanti <massi@miniMac.local>
2021-03-21 15:27:12 +0100

    FEATURE: support for 1Hz and 1kHz ticks

--- ticker.h
+++ ticker.h
@@ -1,7 +1,7 @@
 /*
  * ticker.h
  *
- *  v1.1
+ *  v1.2
  *
  *  MIT License
  *  Massimiliano Galanti <massimiliano.galanti@gmail.com>
@@ -31,7 +31,7 @@
 #ifndef GetSysCount
 /* Platform dependent */
 
-#include "stm32f4xx_hal.h"
+#include "main.h"
 
 #define GetSysCount() HAL_GetTick()
 #define SYSCNTxMS 1
@@ -39,12 +39,15 @@
 /* Platform Independent */
 
 #define syscntPERIOD_Hz1    (1000 * SYSCNTxMS)
+#define syscntPERIOD_Hz2    ( 500 * SYSCNTxMS)
 #define syscntPERIOD_Hz5    ( 200 * SYSCNTxMS)
 #define syscntPERIOD_Hz10   ( 100 * SYSCNTxMS)
+#define syscntPERIOD_Hz20   (  50 * SYSCNTxMS)
 #define syscntPERIOD_Hz50   (  20 * SYSCNTxMS)
 #define syscntPERIOD_Hz100  (  10 * SYSCNTxMS)
 #define syscntPERIOD_Hz200  (   5 * SYSCNTxMS)
 #define syscntPERIOD_Hz500  (   2 * SYSCNTxMS)
+#define syscntPERIOD_Hz1000  (   1 * SYSCNTxMS)
 
 struct ticker_s;
 
@@ -69,26 +72,32 @@ typedef struct {
 typedef struct ticker_s {
 	uint32_t now;
 	uint32_t tick1;
+	uint32_t tick2;
 	uint32_t tick5;
 	uint32_t tick10;
+	uint32_t tick20;
 	uint32_t tick50;
 	uint32_t tick100;
 	uint32_t tick200;
 	uint32_t tick500;
+	uint32_t tick1000;
 
 	uint8_t Hz1 :1;
+	uint8_t Hz2 :1;
 	uint8_t Hz5 :1;
 	uint8_t Hz10 :1;
+	uint8_t Hz20 :1;
 	uint8_t Hz50 :1;
 	uint8_t Hz100 :1;
 	uint8_t Hz200 :1;
 	uint8_t Hz500 :1;
+	uint8_t Hz1000 :1;
 
 	ticker_task_t tasks[TICKER_MAX_TASKS];
 } ticker_t;
 
 static inline void tickerInit(ticker_t *t) {
-	t->now = t->tick1 = t->tick5 = t->tick10 = t->tick50 = t->tick100 = t->tick200 = t->tick500 =
+	t->now = t->tick1 = t->tick2 = t->tick5 = t->tick10 = t->tick20 = t->tick50 = t->tick100 = t->tick200 = t->tick500 = t->tick1000 =
 	GetSysCount();
 }
 
@@ -133,7 +142,12 @@ static inline void tickerTick(ticker_t *t) {
 		}
 	}
 
-	t->Hz1 = t->Hz5 = t->Hz10 = t->Hz50 = t->Hz100 = t->Hz200 = t->Hz500 = 0;
+	t->Hz1 = t->Hz2 =t->Hz5 = t->Hz10 = t->Hz20 = t->Hz50 = t->Hz100 = t->Hz200 = t->Hz500 = t->Hz1000 = 0;
+
+	if (DIFFU32(t->now, t->tick1000) >= syscntPERIOD_Hz1000) {
+		t->Hz1000 = 1;
+		t->tick1000 += syscntPERIOD_Hz1000;
+	}
 
 	if (DIFFU32(t->now, t->tick500) >= syscntPERIOD_Hz500) {
 		t->Hz500 = 1;
@@ -155,6 +169,11 @@ static inline void tickerTick(ticker_t *t) {
 		t->tick50 += syscntPERIOD_Hz50;
 	}
 
+	if (DIFFU32(t->now, t->tick20) >= syscntPERIOD_Hz20) {
+		t->Hz20 = 1;
+		t->tick20 += syscntPERIOD_Hz20;
+	}
+
 	if (DIFFU32(t->now, t->tick10) >= syscntPERIOD_Hz10) {
 		t->Hz10 = 1;
 		t->tick10 += syscntPERIOD_Hz10;
@@ -164,6 +183,12 @@ static inline void tickerTick(ticker_t *t) {
 		t->Hz5 = 1;
 		t->tick5 += syscntPERIOD_Hz5;
 	}
+
+	if (DIFFU32(t->now, t->tick2) >= syscntPERIOD_Hz2) {
+		t->Hz2 = 1;
+		t->tick2 += syscntPERIOD_Hz2;
+	}
+
 
 	if (DIFFU32(t->now, t->tick1) >= syscntPERIOD_Hz1) {
 		t->Hz1 = 1;
